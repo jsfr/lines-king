216079dafa0ebeec761d5dc5344567e12505e6f6
Jens Fredskov <jensfredskov@protonmail.ch>
2017-01-14 23:40:42 +0100

    Add multiplayer

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,19 @@ use piston::input::*;
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{ GlGraphics, OpenGL };
 
-const RED:     [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-const GREEN:   [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-const BLUE:    [f32; 4] = [0.0, 0.0, 1.0, 1.0];
-const YELLOW:  [f32; 4] = [0.0, 1.0, 1.0, 1.0];
-const BLACK:   [f32; 4] = [0.0, 0.0, 0.0, 1.0];
-
 type Point = (u8, u8);
+type Color = [f32; 4];
+
+const RED:    Color = [1.0, 0.0, 0.0, 1.0];
+const GREEN:  Color = [0.0, 1.0, 0.0, 1.0];
+const BLUE:   Color = [0.0, 0.0, 1.0, 1.0];
+const YELLOW: Color = [0.0, 1.0, 1.0, 1.0];
+const BLACK:  Color = [0.0, 0.0, 0.0, 1.0];
+
+const BOARD_WIDTH: u8 = 100;
+const BOARD_HEIGHT: u8 = 100;
+const TILE_SIZE: u8 = 4;
+const UPDATE_TIME: f64 = 0.025;
 
 #[derive(Copy, Clone)]
 enum Direction {
@@ -25,8 +31,14 @@ enum Direction {
     West
 }
 
+#[derive(Copy, Clone)]
+enum Tile {
+    Empty,
+    Occupied(Color)
+}
+
 struct Board {
-    tiles: Vec<Vec<bool>>,
+    tiles: Vec<Vec<Tile>>,
     width: u8,
     height: u8
 }
@@ -34,23 +46,67 @@ struct Board {
 struct Player {
     pos: Point,
     color: [f32; 4],
-    direction: Direction
+    direction: Direction,
+    buttons: (Button, Button)
 }
 
 struct App {
     gl: GlGraphics, // OpenGL drawing backend.
-    player: Player,
+    players: Vec<Player>,
     board: Board,
     time: f64,
     update_time: f64,
     tile_size: u8
 }
 
+// fn mod(a, b) {
+//     ((a % b) + b) % b
+// }
+
+impl Player {
+    fn turn(&mut self, button: &Button) {
+        if self.buttons.0 == *button {
+            self.turn_left()
+        } else if self.buttons.1 == *button {
+            self.turn_right()
+        }
+    }
+
+    fn turn_left(&mut self) {
+        use Direction::*;
+        match self.direction {
+            North => self.direction = West,
+            South => self.direction = East,
+            East => self.direction = North,
+            West => self.direction = South
+        }
+    }
+
+    fn turn_right(&mut self) {
+        use Direction::*;
+        match self.direction {
+            North => self.direction = East,
+            South => self.direction = West,
+            East => self.direction = South,
+            West => self.direction = North
+        }
+    }
+
+    fn step(&mut self) {
+        match self.direction {
+            Direction::North => self.pos.1 -= 1,
+            Direction::South => self.pos.1 += 1,
+            Direction::West => self.pos.0 -= 1,
+            Direction::East => self.pos.0 += 1,
+        }
+    }
+}
+
 impl Board {
-    fn new(width: u8, height: u8) -> Board {
-        let mut tiles = Vec::new();
-        for _ in 0..width-1 {
-            tiles.push(vec![false; height as usize]);
+    fn new(width: u8, height: u8, players: &Vec<Player>) -> Board {
+        let mut tiles = vec![vec![Tile::Empty; height as usize]; width as usize];
+        for player in players {
+            tiles[player.pos.0 as usize][player.pos.1 as usize] = Tile::Occupied(player.color);
         }
         Board {
             width: width,
@@ -62,24 +118,17 @@ impl Board {
 
 impl App {
     fn render(&mut self, args: &RenderArgs) {
-        use graphics::*;
-
-        let x = self.player.pos.0 as f64;
-        let y = self.player.pos.1 as f64;
-        let color = self.player.color;
         let tile_size = self.tile_size as f64;
-        let player_square = rectangle::square(x * tile_size, y * tile_size, tile_size);
         let width = self.board.width;
         let height = self.board.height;
         let ref tiles = self.board.tiles;
 
-
         self.gl.draw(args.viewport(), |context, gl| {
             clear(BLACK, gl);
-            rectangle(color, player_square, context.transform, gl);
+            // rectangle(color, player_square, context.transform, gl);
             for x in 0..width-1 {
                 for y in 0..height-1 {
-                    if tiles[x as usize][y as usize] {
+                    if let Tile::Occupied(color) = tiles[x as usize][y as usize] {
                         let square = rectangle::square(
                             x as f64 * tile_size,
                             y as f64 * tile_size,
@@ -97,40 +146,19 @@ impl App {
 
         while self.time > self.update_time {
             self.time -= self.update_time;
-
-            self.board.tiles[self.player.pos.0 as usize][self.player.pos.1 as usize] = true;
-
-            match self.player.direction {
-                Direction::North => self.player.pos.1 -= 1,
-                Direction::South => self.player.pos.1 += 1,
-                Direction::West => self.player.pos.0 -= 1,
-                Direction::East => self.player.pos.0 += 1,
+            for mut player in &mut self.players {
+                player.step();
+                self.board.tiles[player.pos.0 as usize][player.pos.1 as usize] = Tile::Occupied(player.color);
             }
         }
     }
 
-    fn move_player(&mut self, button: &Button) {
-        use Direction::*;
-
-        match (self.player.direction, *button) {
-            (North, Button::Keyboard(Key::A)) => self.player.direction = West,
-            (North, Button::Keyboard(Key::D)) => self.player.direction = East,
-            (South, Button::Keyboard(Key::A)) => self.player.direction = East,
-            (South, Button::Keyboard(Key::D)) => self.player.direction = West,
-            (East, Button::Keyboard(Key::A)) => self.player.direction = North,
-            (East, Button::Keyboard(Key::D)) => self.player.direction = South,
-            (West, Button::Keyboard(Key::A)) => self.player.direction = South,
-            (West, Button::Keyboard(Key::D)) => self.player.direction = North,
-            _ => ()
+    fn press_button(&mut self, button: &Button) {
+        for mut player in &mut self.players {
+            player.turn(button);
         }
     }
 }
-
-
-const BOARD_WIDTH: u8 = 50;
-const BOARD_HEIGHT: u8 = 50;
-const TILE_SIZE: u8 = 10;
-const UPDATE_TIME: f64 = 0.10;
 
 fn main() {
     // Change this to OpenGL::V2_1 if not working.
@@ -139,25 +167,35 @@ fn main() {
     let width = BOARD_WIDTH as u32 * TILE_SIZE as u32;
     let height = BOARD_HEIGHT as u32 * TILE_SIZE as u32;
 
-    // Create an Glutin window.
+    // Create a glutin window.
     let mut window: Window = WindowSettings::new("Lines King", [width, height])
         .opengl(opengl)
         .exit_on_esc(true)
         .build()
         .unwrap();
 
-    let player = Player {
-        pos: (1, 3),
-        color: RED,
-        direction: Direction::East
-    };
+    let players = vec![
+        Player {
+            pos: (1, 1),
+            color: RED,
+            direction: Direction::East,
+            buttons: (Button::Keyboard(Key::A), Button::Keyboard(Key::D))
+        },
+        Player {
+            pos: (10, 10),
+            color: BLUE,
+            direction: Direction::South,
+            buttons: (Button::Keyboard(Key::D9), Button::Keyboard(Key::D0))
+        }
+    ];
 
-    let board = Board::new(BOARD_WIDTH, BOARD_HEIGHT);
+    let board = Board::new(BOARD_WIDTH, BOARD_HEIGHT, &players);
+
 
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        player: player,
+        players: players,
         board: board,
         time: 0.0,
         update_time: UPDATE_TIME,
@@ -175,7 +213,7 @@ fn main() {
         }
 
         if let Some(b) = e.press_args() {
-            app.move_player(&b)
+            app.press_button(&b)
         }
 
     }
